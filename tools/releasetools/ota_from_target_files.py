3a3fde020009cc0531c305894305a444e69a0d63
lubik1 <pofigus1989@gmail.com>
2020-12-27 02:20:41 +0100

    build: Don't call make_recovery_patch

--- tools/releasetools/ota_from_target_files.py
+++ tools/releasetools/ota_from_target_files.py
@@ -536,19 +536,19 @@ def _WriteRecoveryImageToBoot(script, output_zip):
     script.WriteRawImage("/boot", "recovery.img")
 
 
-def HasRecoveryPatch(target_files_zip, info_dict):
-  board_uses_vendorimage = info_dict.get("board_uses_vendorimage") == "true"
+#def HasRecoveryPatch(target_files_zip, info_dict):
+#  board_uses_vendorimage = info_dict.get("board_uses_vendorimage") == "true"
 
-  if board_uses_vendorimage:
-    target_files_dir = "VENDOR"
-  else:
-    target_files_dir = "SYSTEM/vendor"
+#  if board_uses_vendorimage:
+#    target_files_dir = "VENDOR"
+#  else:
+#    target_files_dir = "SYSTEM/vendor"
 
-  patch = "%s/recovery-from-boot.p" % target_files_dir
-  img = "%s/etc/recovery.img" %target_files_dir
+#  patch = "%s/recovery-from-boot.p" % target_files_dir
+#  img = "%s/etc/recovery.img" %target_files_dir
 
-  namelist = [name for name in target_files_zip.namelist()]
-  return (patch in namelist or img in namelist)
+#  namelist = [name for name in target_files_zip.namelist()]
+#  return (patch in namelist or img in namelist)
 
 
 def HasPartition(target_files_zip, partition):
@@ -739,7 +739,7 @@ def WriteFullOTAPackage(input_zip, output_file):
       metadata=metadata,
       info_dict=OPTIONS.info_dict)
 
-  assert HasRecoveryPatch(input_zip, info_dict=OPTIONS.info_dict)
+#  assert HasRecoveryPatch(input_zip, info_dict=OPTIONS.info_dict)
 
   # Assertions (e.g. downgrade check, device properties check).
   ts = target_info.GetBuildProp("ro.build.date.utc")
